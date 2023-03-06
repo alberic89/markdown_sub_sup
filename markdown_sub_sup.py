9c5ad1b66646a4c0e33001f726f30333eab0b6aa
alberic89 <alberic89@gmx.com>
2023-03-06 18:17:19 +0100

    Create markdown_sub_sup.py

---
+++ markdown_sub_sup.py
@@ -0,0 +1,21 @@
+from markdown.extensions import Extension
+from markdown.inlinepatterns import SimpleTagInlineProcessor
+
+SUP_RE = r"(\^)([^\^]+)\2"
+SUB_RE = r"(\~)([^\~]+)\2"
+
+class MarkdownSubSup(Extension):
+    def extendMarkdown(self, md):
+        del_proc = SimpleTagInlineProcessor(SUB_RE, 'sub')
+        md.inlinePatterns.register(del_proc, 'sub', 65)
+        del_proc = SimpleTagInlineProcessor(SUP_RE, 'sup')
+        md.inlinePatterns.register(del_proc, 'sup', 65)
+
+
+def makeExtension(**kwargs):
+    return MarkdownMark(**kwargs)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testfile('README.md')
